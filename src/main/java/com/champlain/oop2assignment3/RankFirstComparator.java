ef5886fce71b5337d8f18e20a54748cb360f9458
Sleepzs <166639475+Sleepzs@users.noreply.github.com>
2024-10-07 18:56:50 -0400

    removing the errors that where added

--- src/main/java/com/champlain/oop2assignment3/RankFirstComparator.java
+++ src/main/java/com/champlain/oop2assignment3/RankFirstComparator.java
@@ -1,17 +1,21 @@
 package com.champlain.oop2assignment3;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 /**
  * Comparator for sorting cards by rank first, then by suit.
  */
 public class RankFirstComparator implements Comparator<Card> {
+
     @Override
     public int compare(Card card1, Card card2) {
-        int rankComparison = card1.getRank().compareTo(card2.getRank());
-        if (rankComparison != 0) {
-            return rankComparison;
-        }
-        return card1.getSuit().compareTo(card2.getSuit());
+        // Null checks for both cards
+        Objects.requireNonNull(card1, "First card must not be null");
+        Objects.requireNonNull(card2, "Second card must not be null");
+
+        return Comparator.comparing(Card::getRank, Comparator.nullsLast(Comparator.naturalOrder()))
+                .thenComparing(Card::getSuit, Comparator.nullsLast(Comparator.naturalOrder()))
+                .compare(card1, card2);
     }
 }
